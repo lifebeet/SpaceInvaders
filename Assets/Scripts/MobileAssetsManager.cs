cffc9d5921e3715b025df74a7a82a90d67c7d6d2
Marcoxs22 <andres.peralta@uteca.edu.mx>
2026-07-22 20:29:45 -0600

    inicio

--- Assets/Scripts/MobileAssetsManager.cs
+++ Assets/Scripts/MobileAssetsManager.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+    using UnityEngine;
 
 public class MobileAssetsManager : MonoBehaviour
 {
@@ -8,16 +8,24 @@ private GameObject[] mobileAssets;
 private GameObject[] desktopAssets;
 private void Awake()
 {
-#if UNITY_IOS || UNITY_ANDROID
-foreach (var asset in mobileAssets)
-{
-asset.SetActive(true);
+if(Application.isEditor)
+        {
+          SetActiveAssets(desktopAssets, true);   
+        }
+        else if (Application.isMobilePlatform)
+        {
+            SetActiveAssets(mobileAssets, true);
+        }
+        else
+        {
+            SetActiveAssets(desktopAssets, true);
+        }      
 }
-#else
-foreach (var item in desktopAssets)
-{
-asset.SetActive(true);
-}
-#endif
-}
+private void SetActiveAssets(GameObject[] assets, bool isActive)
+    {
+        foreach(var asset in assets)
+        {
+            asset.SetActive(isActive);
+        }
+    }
 }
